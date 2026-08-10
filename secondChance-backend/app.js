5d555e2141cb32a907d9d1fb3277d05dd31cdf1a
peter-webs <potaadel01@gmail.com>
2026-08-10 15:07:24 -0400

    Implement search API endpoint

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -29,6 +29,7 @@ const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -45,6 +46,8 @@ app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
+
+app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
